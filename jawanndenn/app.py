88ebd3989a5c90d84745ce476b2a11d991953a44
Sebastian Pipping <sebastian@pipping.org>
2016-11-01 22:02:01 +0100

    Extract app module

---
+++ jawanndenn/app.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2016 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under AGPL v3 or later
+
+from __future__ import print_function
+
+import json
+import os
+import sys
+
+import bottle
+import pkg_resources
+
+from jawanndenn.metadata import APP_NAME
+from jawanndenn.poll import PollDatabase
+
+
+STATIC_HOME_LOCAL = os.path.abspath(os.path.normpath(
+        os.path.join(os.path.dirname(__file__), 'static')
+            if os.path.exists(os.path.join(os.path.dirname(__file__),
+                    '..', 'setup.py')) else
+            pkg_resources.resource_filename(APP_NAME, 'static')
+        ))
+_STATIC_HOME_REMOTE = '/static'
+
+
+db = PollDatabase()
+
+
+def _to_json(e):
+    return json.dumps(e)
+
+
+@bottle.get('/static/<path:path>')
+def _static(path):
+    content_type = {
+                'css': 'text/css',
+                'js': 'application/javascript',
+                'xhtml': 'application/xhtml+xml',
+            }[path.split('.')[-1]]
+    bottle.response.content_type = content_type
+    return bottle.static_file(path, root=STATIC_HOME_LOCAL)
+
+
+@bottle.get('/')
+def _index():
+    bottle.response.content_type = 'application/xhtml+xml'
+    return bottle.static_file('setup.xhtml', root=STATIC_HOME_LOCAL)
+
+
+@bottle.post('/create')
+def _create():
+    config = json.loads(bottle.request.forms['config'])
+    poll_id = db.add(config)
+    bottle.redirect('/poll/%s' % poll_id)
+
+
+@bottle.get('/poll/<poll_id>')
+def _poll(poll_id):
+    poll_id  # mark as used
+    bottle.response.content_type = 'application/xhtml+xml'
+    return bottle.static_file('poll.xhtml', root=STATIC_HOME_LOCAL)
+
+
+@bottle.get('/data/<poll_id>')
+def _data(poll_id):
+    poll = db.get(poll_id)
+    return _to_json({
+        'config': poll.config,
+        'votes': poll.votes,
+    })
+
+
+@bottle.post('/vote/<poll_id>')
+def _vote(poll_id):
+    voterName = bottle.request.forms['voterName']
+    poll = db.get(poll_id)
+    votes = [bottle.request.forms.get('option%d' % i) == 'on'
+            for i in xrange(len(poll.options))]
+    poll.vote(voterName, votes)
+
+    bottle.redirect('/poll/%s' % poll_id)
+
+
+def run_server(options):
+    if options.debug:
+        bottle.debug(True)
+
+    try:
+        bottle.run(
+                host=options.host,
+                port=options.port,
+                server=options.server,
+                )
+    except ImportError:
+        print('ERROR: WSGI server "%s" does not seem to be available.'
+                % options.server,
+                file=sys.stderr)
+        sys.exit(2)
--- jawanndenn/main.py
+++ jawanndenn/main.py
@@ -1,87 +1,11 @@
 # Copyright (C) 2016 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under AGPL v3 or later
 
-from __future__ import print_function
-
 import argparse
 import errno
-import json
 import logging
 import os
 import sys
-
-import bottle
-import pkg_resources
-
-from jawanndenn.metadata import APP_NAME
-from jawanndenn.poll import PollDatabase
-
-
-_STATIC_HOME_LOCAL = os.path.abspath(os.path.normpath(
-        os.path.join(os.path.dirname(__file__), 'static')
-            if os.path.exists(os.path.join(os.path.dirname(__file__),
-                    '..', 'setup.py')) else
-            pkg_resources.resource_filename(APP_NAME, 'static')
-        ))
-_STATIC_HOME_REMOTE = '/static'
-
-
-_db = PollDatabase()
-
-
-def _to_json(e):
-    return json.dumps(e)
-
-
-@bottle.get('/static/<path:path>')
-def static(path):
-    content_type = {
-                'css': 'text/css',
-                'js': 'application/javascript',
-                'xhtml': 'application/xhtml+xml',
-            }[path.split('.')[-1]]
-    bottle.response.content_type = content_type
-    return bottle.static_file(path, root=_STATIC_HOME_LOCAL)
-
-
-@bottle.get('/')
-def index():
-    bottle.response.content_type = 'application/xhtml+xml'
-    return bottle.static_file('setup.xhtml', root=_STATIC_HOME_LOCAL)
-
-
-@bottle.post('/create')
-def create():
-    config = json.loads(bottle.request.forms['config'])
-    poll_id = _db.add(config)
-    bottle.redirect('/poll/%s' % poll_id)
-
-
-@bottle.get('/poll/<poll_id>')
-def poll(poll_id):
-    poll_id  # mark as used
-    bottle.response.content_type = 'application/xhtml+xml'
-    return bottle.static_file('poll.xhtml', root=_STATIC_HOME_LOCAL)
-
-
-@bottle.get('/data/<poll_id>')
-def data(poll_id):
-    poll = _db.get(poll_id)
-    return _to_json({
-        'config': poll.config,
-        'votes': poll.votes,
-    })
-
-
-@bottle.post('/vote/<poll_id>')
-def vote(poll_id):
-    voterName = bottle.request.forms['voterName']
-    poll = _db.get(poll_id)
-    votes = [bottle.request.forms.get('option%d' % i) == 'on'
-            for i in xrange(len(poll.options))]
-    poll.vote(voterName, votes)
-
-    bottle.redirect('/poll/%s' % poll_id)
 
 
 def _require_hash_randomization():
@@ -98,23 +22,6 @@ def _require_hash_randomization():
         os.execve(argv[0], argv, env)
 
 
-def _run_server(options):
-    if options.debug:
-        bottle.debug(True)
-
-    try:
-        bottle.run(
-                host=options.host,
-                port=options.port,
-                server=options.server,
-                )
-    except ImportError:
-        print('ERROR: WSGI server "%s" does not seem to be available.'
-                % options.server,
-                file=sys.stderr)
-        sys.exit(2)
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true')
@@ -127,21 +34,23 @@ def main():
 
     _require_hash_randomization()
 
-    logging.debug('Serving static files from "%s"' % _STATIC_HOME_LOCAL)
+    from jawanndenn.app import db, run_server, STATIC_HOME_LOCAL
+
+    logging.debug('Serving static files from "%s"' % STATIC_HOME_LOCAL)
 
     filename = os.path.expanduser('~/jawanndenn.pickle')
 
     try:
-        _db.load(filename)
+        db.load(filename)
     except IOError as e:
         if e.errno != errno.ENOENT:
             raise
-        _db.save(filename)  # catch saving trouble early
+        db.save(filename)  # catch saving trouble early
 
     try:
-        _run_server(options)
+        run_server(options)
     finally:
-        _db.save(filename)
+        db.save(filename)
 
 
 if __name__ == '__main__':
