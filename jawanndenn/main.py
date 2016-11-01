26ea19ed156f95a219944ac1a59687a4c6f03005
Sebastian Pipping <sebastian@pipping.org>
2016-11-01 23:47:58 +0100

    Enrich --help output (issue #13)

--- jawanndenn/main.py
+++ jawanndenn/main.py
@@ -8,6 +8,29 @@ import os
 import sys
 
 
+_BOTTLE_BACKENDS = (
+    'cgi',
+    'flup',
+    'gae',
+    'wsgiref',
+    'cherrypy',
+    'paste',
+    'rocket',
+    'waitress',
+    'gunicorn',
+    'eventlet',
+    'gevent',
+    'diesel',
+    'fapws3',
+    'tornado',
+    'twisted',
+    'meinheld',
+    'bjoern',
+    'auto',
+)
+
+_DEFAULT_BACKEND = 'paste'
+
 _log = logging.getLogger(__name__)
 
 
@@ -27,10 +50,20 @@ def _require_hash_randomization():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--host', default='127.0.0.1')
-    parser.add_argument('--port', default=8080, type=int)
-    parser.add_argument('--server', default='paste')
+    parser.add_argument('--debug', action='store_true',
+            help='Enable debug mode (default: disabled)')
+    parser.add_argument('--host', default='127.0.0.1', metavar='HOST',
+            help='Hostname or IP address to listen at (default: %(default)s)')
+    parser.add_argument('--port', default=8080, type=int, metavar='PORT',
+            help='Port to listen at (default: %(default)s)')
+    parser.add_argument('--server', default='paste', metavar='BACKEND',
+            help='bottle backend to use (default: %%(default)s)'
+                '; as of this writing bottle supports: %s. '
+                'For the most current list, please check the documentation '
+                'of bottle.'
+                % ', '.join(sorted(b for b in _BOTTLE_BACKENDS
+                                   if b != _DEFAULT_BACKEND))
+                )
     options = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG if options.debug else logging.INFO)
