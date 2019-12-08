7c8e1a9f68872432ca333f182da44f34e662010f
Sebastian Pipping <sebastian@pipping.org>
2019-12-08 17:38:04 +0100

    views: Handle malformed JSON during poll creation

--- jawanndenn/views.py
+++ jawanndenn/views.py
@@ -3,6 +3,7 @@
 
 import json
 from functools import wraps
+from json import JSONDecodeError
 
 from django.conf import settings
 from django.db import transaction
@@ -41,7 +42,11 @@ def index_get_view(request):
 
 @require_POST
 def poll_post_view(request):
-    config = json.loads(request.POST.get('config', '{}'))
+    try:
+        config = json.loads(request.POST.get('config', '{}'))
+    except JSONDecodeError:
+        return HttpResponseBadRequest('Poll configuration is not '
+                                      'well-formed JSON')
     poll_equal_width = bool(config.get('equal_width', False))
     poll_title = safe_html(config.get('title', ''))
     poll_option_names = map(safe_html, config.get('options', []))
