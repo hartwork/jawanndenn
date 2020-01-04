9e53bdc4b8acd6df2280b957b75d10415a11e619
Sebastian Pipping <sebastian@pipping.org>
2020-01-04 23:20:09 +0100

    settings: Enable Django management command "show_urls"

--- jawanndenn/settings.py
+++ jawanndenn/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',  # for management command "show_urls"
     'jawanndenn',
 ]
 
