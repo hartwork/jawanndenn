a0d7a855a2e28c2de18429bfdc75914ca2bd167a
Sebastian Pipping <sebastian@pipping.org>
2025-06-05 17:59:32 +0200

    frontend: rsbuild: Redirect output to Django locations

--- jawanndenn/frontend/rsbuild.config.ts
+++ jawanndenn/frontend/rsbuild.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  output: {
+    assetPrefix: '.', // i.e. turn absolute "/static" into relative "./static"
+    distPath: {
+      root: '../', // this was "dist" before
+    },
+  },
   plugins: [pluginReact()],
 });
