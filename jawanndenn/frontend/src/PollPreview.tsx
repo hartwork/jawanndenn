0fc6b4e86d1dd6a15a828064ffc8fe94db4fef32
Sebastian Pipping <sebastian@pipping.org>
2025-06-09 17:31:21 +0200

    frontend: Make sure the preview demos (few but) at least one "maybe" vote

--- jawanndenn/frontend/src/PollPreview.tsx
+++ jawanndenn/frontend/src/PollPreview.tsx
@@ -6,12 +6,27 @@ import { Poll, PollConfig } from './Poll.tsx';
 const EXAMPLE_PEOPLE = ['Dmitri', 'Jule', 'Vered', 'Matthieu'];
 
 const randomVoteCache = {};
+let randomVotesAny = 0;
+let randomVotesNull = 0;
 
 const getCachedRandomVote = (person, option) => {
   const key = `${person}@${option}`;
   let value: bool | undefined = randomVoteCache[key];
   if (value === undefined) {
-    value = Math.random() > 0.5;
+    const commonCoiceCount = 4;
+    const forceOneMaybeAfter = (EXAMPLE_PEOPLE.length * commonCoiceCount) / 2;
+    if (randomVotesAny > forceOneMaybeAfter && randomVotesNull == 0) {
+      value = null;
+    } else {
+      const zeroToOne = Math.random();
+      value = zeroToOne > 0.95 ? null : zeroToOne > 0.95 / 2 ? true : false;
+    }
+
+    randomVotesAny += 1;
+    if (value === null) {
+      randomVotesNull += 1;
+    }
+
     randomVoteCache[key] = value;
   }
   return value;
